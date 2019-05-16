5abd75dc8831388055d2a7678a456200c72f21f3
oladipo tomide <ayotomiiide@gmail.com>
2019-05-16 04:04:06 +0100

    landing page setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
+import Logo from "./../components/icons/Logo"
+import React from "react"
+
 function Home() {
-    return <div>Welcome to Next.js!</div>;
-  }
-  
-  export default Home;
+  return (
+    <section className="body">
+      <Logo />
+      <div className="hero">
+        <h2>Hi, I'm Tomide Oladipo</h2>
+        <p>A UI / UX Designer & Frontend Dev</p>
+      </div>
+      <style>
+        {`
+        @import url('https://fonts.googleapis.com/css?family=Lato:300,400,700');
+
+          body,html{
+            margin: 0;
+            padding: 0;
+            font-family: "Lato";
+          }
+
+          
+          `}
+      </style>
+
+      <style jsx>
+        {`
+          .body {
+            background-color: black;
+            padding: 20px;
+          }
+
+          .hero {
+            color: white;
+            padding-bottom: 80px;
+          }
+        `}
+      </style>
+    </section>
+  )
+}
+
+export default Home
